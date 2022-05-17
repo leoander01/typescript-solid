0b10dcf13088c85c4801c6ddfd24382d1b33d3d6
leoander01 <leoanderneves8@outlook.com>
2022-05-17 11:06:06 -0300

    Single Responsibility Principle

---
+++ src/srp/entities/shopping-cart.ts
@@ -0,0 +1,32 @@
+import { CartItem } from '../entities/interface/cart-item'
+
+export class ShoppingCart {
+  private readonly _items: CartItem[] = []
+
+  addItems(item: CartItem): void {
+    this._items.push(item)
+  }
+
+  removeItem(index: number): void {
+    this._items.splice(index, 1)
+  }
+
+  get items(): Readonly<CartItem[]> {
+    return this._items
+  }
+
+  total(): number {
+    return +this._items
+      .reduce((total, next) => total + next.price, 0)
+      .toFixed(2)
+  }
+
+  isEmpty(): boolean {
+    return this._items.length === 0
+  }
+
+  clear(): void {
+    console.log('Carrinho de compras foi limpo')
+    this._items.length = 0
+  }
+}
