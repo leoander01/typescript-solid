5e0eecad2059b1a0b3235a2674ec3116bf251337
leoander01 <leoanderneves8@outlook.com>
2022-05-21 08:44:05 -0300

    Liskov Substitution Principle

--- src/lsp/index.ts
+++ src/lsp/index.ts
@@ -1,3 +1,14 @@
+/*
+Liskov substitution principle (Princípio da substituição de Liskov) -
+Se ϕ(x) é uma propriedade demonstrável dos objetos x de tipo T.
+Então ϕ(y)  deve ser verdadeiro para objetos y de tipo S onde S é um subtipo de T.
+
+Mais simples: Subtipos precisam ser substituíveis por seus tipos de base.
+
+Mais simples ainda: Se meu programa espera um Animal, algo do tipo
+Cachorro (que herda de Animal) deve servir como qualquer outro Animal.
+*/
+
 import { Messaging } from './services/messaging'
 import { Persistency } from './services/persistency'
 import { Order } from './classes/order'
