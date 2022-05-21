9e090a7efe83ed2d1ca80e6fab83c47cef826d68
leoander01 <leoanderneves8@outlook.com>
2022-05-21 09:22:24 -0300

    Interface Segregation Principle

---
+++ src/isp/classes/order.ts
@@ -0,0 +1,36 @@
+import { OrderStatus } from './interface/order-status'
+import { Messaging } from '../services/messaging'
+import { Persistency } from '../services/persistency'
+import { ShoppingCart } from './shopping-cart'
+import { CustomerOrder } from './interface/customer-protocol'
+
+export class Order {
+  private _orderStatus: OrderStatus = 'open'
+
+  constructor(
+    private readonly cart: ShoppingCart,
+    private readonly messaging: Messaging,
+    private readonly persistency: Persistency,
+    private readonly customer: CustomerOrder,
+  ) {}
+
+  get orderStatus(): OrderStatus {
+    return this._orderStatus
+  }
+
+  checkout(): void {
+    if (this.cart.isEmpty()) {
+      console.log('Seu carrinho está vazio')
+      return
+    }
+
+    this._orderStatus = 'closed'
+    this.messaging.sendMessage(
+      `Seu pedido com total de ${this.cart.totalWithDiscount()} foi recebido`,
+    )
+    this.persistency.saveOrder()
+    this.cart.clear()
+
+    console.log('O cliente é:', this.customer.getName(), this.customer.getIDN())
+  }
+}
