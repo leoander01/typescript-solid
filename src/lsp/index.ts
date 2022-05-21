e8f906a1ca6c0bc0afc87ab14d7aa0829c7c1718
leoander01 <leoanderneves8@outlook.com>
2022-05-21 08:43:26 -0300

    Liskov Substitution Principle

---
+++ src/lsp/index.ts
@@ -0,0 +1,29 @@
+import { Messaging } from './services/messaging'
+import { Persistency } from './services/persistency'
+import { Order } from './classes/order'
+import { Product } from './classes/product'
+import { ShoppingCart } from './classes/shopping-cart'
+import {
+  FiftyPercentDiscount,
+  TenPercentDiscount,
+  NoDiscount,
+} from './classes/discount'
+
+const fiftyPercentDiscount = new FiftyPercentDiscount()
+const tenPercentDiscount = new TenPercentDiscount()
+const noDiscount = new NoDiscount()
+const shoppingCart = new ShoppingCart(tenPercentDiscount)
+const messaging = new Messaging()
+const persistency = new Persistency()
+const order = new Order(shoppingCart, messaging, persistency)
+
+shoppingCart.addItems(new Product('Camiseta', 49.91))
+shoppingCart.addItems(new Product('Caderno', 9.9123))
+shoppingCart.addItems(new Product('Lápis', 1.59))
+
+console.log(shoppingCart.items)
+console.log(shoppingCart.total())
+console.log(shoppingCart.totalWithDiscount())
+console.log(order.orderStatus)
+order.checkout()
+console.log(order.orderStatus)
